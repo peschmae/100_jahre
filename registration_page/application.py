acbc6bfa6b9bce69c051202dd01cc88a7f2f1347
Mathias Petermann <mathias.petermann@gmail.com>
2018-07-21 18:59:20 +0200

    Implemented form

--- registration_page/application.py
+++ registration_page/application.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .config import configure_app
+import pymysql.cursors
 
 
 app = Flask(__name__)
@@ -11,14 +12,54 @@ configure_app(app)
 
 @app.route("/")
 def index():
+    # Connect to the database
+    connection = pymysql.connect(host=app.config['DATABASE_SERVER'],
+                                 user=app.config['DATABASE_USER'],
+                                 password=app.config['DATABASE_PASSWORD'],
+                                 db=app.config['DATABASE_NAME'],
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
     app.logger.debug('index called')
     # todo: fetch list of already commited meals
-    return render_template('index.html')
+    try:
+        with connection.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT `buffet` FROM `registrations` WHERE `buffet`<>''"
+            cursor.execute(sql)
+            meals = cursor.fetchall()
+    finally:
+        connection.close()
+    return render_template('index.html',
+                           meals=meals
+                           )
 
 
 @app.route("/registration-complete", methods=['POST'])
 def registration():
+    # Connect to the database
+    connection = pymysql.connect(host=app.config['DATABASE_SERVER'],
+                                 user=app.config['DATABASE_USER'],
+                                 password=app.config['DATABASE_PASSWORD'],
+                                 db=app.config['DATABASE_NAME'],
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
     app.logger.debug('form posted')
     # todo: write all that shit into the database & maybe send an email
-    return render_template('form.html')
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "INSERT INTO `registrations` (`name`, `companions`, `buffet`, `vegetarian`) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (
+                                 request.form.get('name'),
+                                 request.form.get('companions', default=0),
+                                 request.form.get('buffet'),
+                                 request.form.get('vegetarian', default=0),
+                                )
+                           )
 
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+        connection.commit()
+    finally:
+        connection.close()
+    return render_template('registration.html')
--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `registrations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_bin NOT NULL,
-  `compansions` SMALLINT COLLATE utf8_bin NOT NULL,
-  `buffet` varchar(255) COLLATE utf8_bin NOT NULL,
-  `vegetarian` SMALLINT COLLATE utf8_bin NOT NULL,
+  `companions` SMALLINT COLLATE utf8_bin default 0,
+  `buffet` varchar(255) COLLATE utf8_bin default '',
+  `vegetarian` SMALLINT COLLATE utf8_bin default 0,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 AUTO_INCREMENT=1 ;
